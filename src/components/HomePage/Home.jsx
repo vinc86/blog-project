bee28171ddc466ad00fda1d93958b9c158e597b4
vinc86 <vincenzomancuso86@gmail.com>
2020-09-22 14:59:50 +0200

    blog edit

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -19,13 +19,17 @@ const Home=()=> {
         featuresMenu += openFeatures && "scale";
         return(
             <div className={featuresMenu}>
-                <button className="features-btn" onClick={()=>setAuthor(!author)}><i className="fas fa-feather"></i> </button>
-                <button className="features-btn" onClick={()=>setCategory(!category)}><i className="fas fa-layer-group"></i></button>
-                <button className="features-btn" onClick={()=>setDescription(!description)}><i className="far fa-list-alt"></i></button>
-                <label htmlFor="file">
-                <i className="fas fa-camera-retro features-btn"></i>
+                <button className="features-btn author-hover" onClick={()=>setAuthor(!author)}><i className="fas fa-feather"></i> </button>
+                <div className="btn-author">Add Author</div>
+                <button className="features-btn category-hover" onClick={()=>setCategory(!category)}><i className="fas fa-layer-group"></i></button>
+                <div className="btn-cat">Category</div>
+                <button className="features-btn description-hover" onClick={()=>setDescription(!description)}><i className="far fa-list-alt"></i></button>
+                <div className="btn-descr">Description</div>
+                <label className="image-hover" htmlFor="file">
+                <i className="fas fa-camera-retro features-btn "></i>
                         <input onChange={(e)=>setNewArticle({...newArticle, "image": e.target.files[0]})} type="file" id="file" name="image"/>
                 </label> 
+                <div className="btn-image">Add Image</div>
             </div>
         )
     }
@@ -38,14 +42,17 @@ const Home=()=> {
                 <form onSubmit={saveNewArticle} className="form-container">
                     <input onChange={(e)=>setNewArticle({...newArticle, "title": e.target.value})} type="text" name="title" placeholder="Title..."/>
                     <div className="menu-container">
-                    <button onClick={()=>setOpenFeatures(!openFeatures)} className="features-menu"><i className="fas fa-plus-circle"></i></button>
+                    <button onClick={()=>setOpenFeatures(!openFeatures)} className="features-menu">
+                        <i className="fas fa-plus-circle"></i>
+                    </button>
+                    <div className="features-descr">Add something to your story</div>
                         <div className="sub-container">
-                            {console.log(features)}
+                            {features()}
                         </div>
                     </div>
-                    {author &&<input onChange={(e)=>setNewArticle({...newArticle, "author": e.target.value})} type="text" name="author" placeholder="Author..."/>}
-                    {category && <input onChange={(e)=>setNewArticle({...newArticle, "category": e.target.value.toLowerCase()})} type="text" name="category" placeholder="Category (e.g. Javascript, React..)"/>}
-                    {description && <input onChange={(e)=>setNewArticle({...newArticle, "preview": e.target.value})} type="text" name="preview" placeholder="Description..."/>}
+                    {author &&<input className="inputs" onChange={(e)=>setNewArticle({...newArticle, "author": e.target.value})} type="text" name="author" placeholder="Author..."/>}
+                    {category && <input className="inputs" onChange={(e)=>setNewArticle({...newArticle, "category": e.target.value.toLowerCase()})} type="text" name="category" placeholder="Category (e.g. Javascript, React..)"/>}
+                    {description && <input className="inputs"onChange={(e)=>setNewArticle({...newArticle, "preview": e.target.value})} type="text" name="preview" placeholder="Description..."/>}
                     <textarea onChange={(e)=>setNewArticle({...newArticle, "firstParagraph": e.target.value})} name="paragraph" placeholder="Tell your story..."></textarea>
                     <button type="submit">Save</button>
                 </form>
