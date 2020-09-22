817d1ba3cc2f9055759abf603fbfec6db187a6c6
vinc86 <vincenzomancuso86@gmail.com>
2020-09-22 15:08:14 +0200

    Updates

---
+++ precache-manifest.1720b4d4cc6feb928c43b85f46ef5c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fc474a1b3f81abbe5fc36749ad88dc",
+    "url": "/blog-project/index.html"
+  },
+  {
+    "revision": "d7836581b6e5f57e7a65",
+    "url": "/blog-project/static/css/main.23aa4c23.chunk.css"
+  },
+  {
+    "revision": "538f1fe6454083596a5d",
+    "url": "/blog-project/static/js/2.7d276476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-project/static/js/2.7d276476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7836581b6e5f57e7a65",
+    "url": "/blog-project/static/js/main.1ea8d0aa.chunk.js"
+  },
+  {
+    "revision": "6e56fee545a5f8a3322e",
+    "url": "/blog-project/static/js/runtime-main.4b64186b.js"
+  },
+  {
+    "revision": "7c2b8b585d2a09c7fbbba332bcc5ddfd",
+    "url": "/blog-project/static/media/empty-box.7c2b8b58.png"
+  }
+]);
